307410f4e8e3d537620c290da9ad0e05f39e872b
Alapottra <alapottratrojon@gmail.com>
2022-09-02 21:36:55 +0600

    added the add user functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 
 module.exports.getRandomUser = (req, res) => {
 
-     fs.readFile('users.json','utf-8' ,function (err, data) {
+    fs.readFile('users.json', 'utf-8', function (err, data) {
         if (err) throw err;
 
-        
+
         const allusers = JSON.parse(data);
 
         const randomNumber = Math.floor(Math.random() * allusers.length); + 1
         const randomUser = allusers[randomNumber];
 
-        res.json({randomUser : randomUser});
+        res.json({ randomUser: randomUser });
 
-       
-        
+
+
     });
 
 
@@ -22,24 +22,48 @@ module.exports.getRandomUser = (req, res) => {
 
 module.exports.getAllUsers = (req, res) => {
 
-    fs.readFile('users.json','utf-8' ,function (err, data) {
+    fs.readFile('users.json', 'utf-8', function (err, data) {
         if (err) throw err;
 
-        
+
         const allusers = JSON.parse(data);
 
-        res.json({allusers})
+        res.json({ allusers })
 
-       
-        
+
+
     });
 
-   
+
 }
 
 
 module.exports.saveUser = (req, res) => {
-    res.json({ message: 'user save route' })
+
+    const  user  = req.body;
+
+    if (!user?.id || !user?.gender || !user?.name || !user?.contact || !user?.address || !user?.photoUrl) {
+        res.status(500).send({ message : 'required properties of the user is missing'});
+        throw new Error('required properties of the user is missing');
+    }
+
+
+    fs.readFile('users.json', function (err, data) {
+
+        const users = JSON.parse(data)
+        users.push(user);
+        console.log(users);
+        fs.writeFile("users.json", JSON.stringify(users), (err) => {
+            if(err){
+                console.log(err);
+            }
+            res.json({ message: 'user has been added' })
+        })
+       
+    })
+
+
+
 }
 
 
