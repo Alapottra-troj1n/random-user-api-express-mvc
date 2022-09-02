a9d4068fd5e48ed89c18205fd056a6360b5afa72
Alapottra <alapottratrojon@gmail.com>
2022-09-02 18:14:33 +0600

    added basic routes and controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,28 @@
 module.exports.getRandomUser = (req, res) => {
     res.json({message: 'random user'})
 }
+
+module.exports.getAllUsers = (req, res) => {
+    res.json({message: 'all users'})
+}
+
+
+module.exports.saveUser = (req, res) => {
+    res.json({message: 'user save route'})
+}
+
+
+module.exports.updateUser = (req, res) => {
+    res.json({message: 'user updated'})
+}
+
+
+
+module.exports.bulkUpate = (req, res) => {
+    res.json({message: 'users bulk update'})
+}
+
+
+module.exports.deleteUser = (req, res) => {
+    res.json({message: 'user delete'})
+}
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -7,6 +7,16 @@ const router = express.Router();
 
 router.route('/random').get(userController.getRandomUser);
 
+router.route('/all').get(userController.getAllUsers);
+
+router.route('/save').post(userController.saveUser);
+
+router.route('/update').patch(userController.updateUser);
+
+router.route('/bulk-update').patch(userController.bulkUpate);
+
+router.route('/delete').delete(userController.deleteUser);
+
 
 
 module.exports = router;
