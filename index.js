609f02a8806966a86d592a9d20b61a4989531286
Alapottra <alapottratrojon@gmail.com>
2022-09-02 23:16:29 +0600

    added port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const userRoutes = require('./routes/v1/user.route');
 
 //global middleware
