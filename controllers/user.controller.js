d98f6b441214806cd1eead76ee2b998a82c1d30d
Alapottra <alapottratrojon@gmail.com>
2022-09-02 19:15:02 +0600

    all users response done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,28 +1,43 @@
+const fs = require('fs');
+
 module.exports.getRandomUser = (req, res) => {
-    res.json({message: 'random user'})
+    res.json({ message: 'random user' })
 }
 
 module.exports.getAllUsers = (req, res) => {
-    res.json({message: 'all users'})
+
+    fs.readFile('users.json','utf-8' ,function (err, data) {
+        if (err) throw err;
+
+        
+        const allusers = JSON.parse(data);
+
+        res.json({allusers})
+
+       
+        
+    });
+
+   
 }
 
 
 module.exports.saveUser = (req, res) => {
-    res.json({message: 'user save route'})
+    res.json({ message: 'user save route' })
 }
 
 
 module.exports.updateUser = (req, res) => {
-    res.json({message: 'user updated'})
+    res.json({ message: 'user updated' })
 }
 
 
 
 module.exports.bulkUpate = (req, res) => {
-    res.json({message: 'users bulk update'})
+    res.json({ message: 'users bulk update' })
 }
 
 
 module.exports.deleteUser = (req, res) => {
-    res.json({message: 'user delete'})
+    res.json({ message: 'user delete' })
 }
